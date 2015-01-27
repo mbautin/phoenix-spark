cba6afb469a75489330d8170467e69abe2a523d4
Mikhail Bautin <mbautin@gmail.com>
2015-01-27 01:01:11 -0800

    More hacks to make it work against a remote Phoenix 4.2.2 cluster with HBase 0.98.9 and Hadoop 2.5.0 (CDH 5.3.0).

--- src/main/scala/com/simplymeasured/spark/PhoenixRDD.scala
+++ src/main/scala/com/simplymeasured/spark/PhoenixRDD.scala
@@ -26,13 +26,13 @@ import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.expressions.GenericMutableRow
 import org.apache.spark.sql.catalyst.types._
-import org.apache.spark.sql.{SQLContext, SchemaRDD}
+import org.apache.spark.sql.{Row, SQLContext, SchemaRDD}
 
 import scala.collection.JavaConverters._
 
 class PhoenixRDD(sc: SparkContext, table: String, columns: Seq[String],
                  predicate: Option[String] = None, @transient conf: Configuration)
-  extends RDD[PhoenixRecord](sc, Nil) with Logging {
+  extends RDD[Row](sc, Nil) with Logging {
 
   @transient lazy val phoenixConf = {
     getPhoenixConfiguration
@@ -54,7 +54,9 @@ class PhoenixRDD(sc: SparkContext, table: String, columns: Seq[String],
 
   @DeveloperApi
   override def compute(split: Partition, context: TaskContext) = {
-    phoenixRDD.compute(split, context).map(r => r._2)
+    phoenixRDD.compute(split, context).map { r =>
+      new GenericMutableRow(r._2.getValues.toArray.asInstanceOf[Array[Any]])
+    }
   }
 
   def printPhoenixConfig(conf: Configuration): Unit = {
@@ -99,20 +101,7 @@ class PhoenixRDD(sc: SparkContext, table: String, columns: Seq[String],
 
     val structFields = phoenixSchemaToCatalystSchema(selectColumnMetadataList)
 
-    sqlContext.applySchema(map(pr => {
-      val values = pr.getValues.asScala.toIndexedSeq
-
-      val r = new GenericMutableRow(values.size)
-
-      var i = 0
-      for (columnName <- columnNames) {
-        r.update(i, values(i))
-
-        i += 1
-      }
-
-      r
-    }), new StructType(structFields))
+    sqlContext.applySchema(this, new StructType(structFields))
   }
 
   def phoenixSchemaToCatalystSchema(columnList: Seq[ColumnInfo]) = {
@@ -183,7 +172,7 @@ class PhoenixRDD(sc: SparkContext, table: String, columns: Seq[String],
 
 object PhoenixRDD {
   def NewPhoenixRDD(sc: SparkContext, table: String, columns: Seq[String],
-                    predicate: Option[String] = None, conf: Configuration) = {
+            predicate: Option[String] = None, conf: Configuration) = {
     new PhoenixRDD(sc, table, columns, predicate, conf)
   }
 }
